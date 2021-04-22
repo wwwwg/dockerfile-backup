c063d80fdfcd2edfbe07c2c47054487bb64e2950
Yocents <yocents@gmail.com>
2021-04-22 23:05:28 +0800

    add git:web

---
+++ git/entrypoint.web.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+readonly conf_dir="/etc/lighttpd"
+readonly data_dir="/data"
+
+getent group lighttpd >/dev/null || \
+    addgroup -g ${PGID:-1000} lighttpd 2>/dev/null
+getent passwd lighttpd >/dev/null || \
+    adduser -D -H -h ${data_dir} -s /sbin/nologin -G lighttpd -u ${PUID:-1000} lighttpd 2>/dev/null
+
+set -eu
+
+chown -R lighttpd:lighttpd /var/log/lighttpd
+
+# Check the configuration file before running
+/usr/sbin/lighttpd -f ${conf_dir}/lighttpd.conf -t >/dev/null
+
+exec "$@"
